34b980146a6678dea6aaf1903e43a6e3edcc1c9c
MohammedBrunfinger <szholowczak@gmail.com>
2021-02-15 17:04:43 +0100

    boll

--- main.lua
+++ main.lua
@@ -1,3 +1,28 @@
+x = 100
+y = 485
+
+function jumpT()
+  y = y - 10
+  if y == y - 10 then
+    y = y + 10
+  end
+end
+
 function love.draw ()
-    love.graphics.circle('fill',0,0,15)
+  love.graphics.setColor(255, 0, 0)
+  love.graphics.circle('fill',x,y,15)
+  love.graphics.setColor(255,255,255)
+  love.graphics.polygon('fill',0,500,0,505,10000,505,10000,500)
+end
+
+function love.update(dt)
+  if love.keyboard.isDown("right") then
+    x = x + 5
+  end
+  if love.keyboard.isDown("left") then
+    x = x - 5
+  end
+  if love.keyboard.isDown("up") then
+    jumpT()
+  end
 end
