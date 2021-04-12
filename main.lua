01fdaf4eb35e19f7de713515d0b26a156442f214
MohammedBrunfinger <szholowczak@gmail.com>
2021-04-12 22:05:27 +0200

    added backroud, more audio, and a secret ester egg

--- main.lua
+++ main.lua
@@ -3,25 +3,33 @@ y = 300
 local Squeres = {}
 local maxSquerers = 2
 local squereScale = 5
-local radius = 15
 local width = 0.05
 local height = 0.05
 myFont = love.graphics.newFont(60)
 local score = 0
 bigChungus = {}
 daBaby = {}
+granChungo = {}
+local player = bigChungus
+
 
 function love.load()
-  bigChungus.image = love.graphics.newImage("big-chungus-jkg.png")
-  daBaby.image = love.graphics.newImage("Happy-Baby-Transparent.png")
-  babyCry = love.audio.newSource("baby-crying-08_01.wav", "stream")
-  biC = love.audio.newSource("bigC.wav", "stream")
-  biCTheme = love.audio.newSource("Big-Big-Chungus.wav", "stream")
+  background = love.graphics.newImage("textures/background-chungus.png")
+  background1 = love.graphics.newImage("textures/background-coockout.png")
+  bigChungus.image = love.graphics.newImage("textures/big-chungus-jkg.png")
+  daBaby.image = love.graphics.newImage("textures/Happy-Baby-Transparent.png")
+  granChungo.image = love.graphics.newImage("textures/gran-chungo.png")
+  babyCry = love.audio.newSource("audio/baby-crying-08_01.wav", "stream")
+  biCTheme = love.audio.newSource("audio/Big-Big-Chungus.wav", "stream")
+  crunch = love.audio.newSource("audio/crunch.wav", "stream")
+  mexicoTheme = love.audio.newSource("audio/mexico-theme.wav", "stream" )
 love.audio.play(biCTheme)
 end
 
 function love.draw ()
-  love.graphics.draw(bigChungus.image, x, y, 0, width, height, bigChungus.image:getWidth() / 2, bigChungus.image:getHeight() / 2)
+  love.graphics.draw(background, 0, 0, 0, 0.79, 0.785)
+
+  love.graphics.draw(player.image, x, y, 0, width, height, bigChungus.image:getWidth() / 2, bigChungus.image:getHeight() / 2)
 
   for i = 1, #Squeres do
     love.graphics.draw(daBaby.image, Squeres[i].X, Squeres[i].Y, 0, 0.1, 0.1, daBaby.image:getWidth() / 2, daBaby.image:getHeight() / 2)
@@ -65,14 +73,21 @@ function love.update(dt)
     local S = Squeres[i]
     if S ~= nil then 
       local distace = math.sqrt(math.pow(x-S.X,2)+math.pow(y-S.Y,2))
-      if distace < squereScale + radius then
+      if distace < squereScale + 15 then
         table.remove(Squeres,i)
         height = height + 0.01
         width = width + 0.01
         score = score + 1
         love.audio.play(babyCry)
+        love.audio.play(crunch)
       end
     end
+  end
+  if score == 15 then
+    love.audio.stop(biCTheme)
+    love.audio.play(mexicoTheme)
+    player = granChungo
+    background = background1
   end
 end
 
